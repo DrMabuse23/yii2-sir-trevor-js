3aedf2ba85e6eda5ad23c34df471e25135c80a9e
Marco Hanowski <its@time2dev.com>
2014-07-29 16:26:15 +0200

    add validation and improve progress

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-'use strict';
+//'use strict';
 
 var program = require('commander');
 var bar = require('progress-bar');
@@ -8,6 +8,7 @@ var mcapFileExchange = require('mcap-file-upload');
 var component = require('./lib/component');
 var notSupported = require('./lib/notSupported');
 var serverconfig = require('mcaprc');
+var mcapApplicationValidation = require('mcap-application-validation');
 
 function commandInfo() {
     console.log('Version', program._version);
@@ -40,29 +41,48 @@ function commandDeploy() {
     if (!server) {
         return notSupported();
     }
+    var dir = process.cwd();
+    if (program.args) {
+        dir = program.args[0];
+    }
 
-    var _bar = bar.create(process.stdout);
-    var uploadConfig = {
-        progress: function (percent) {
-            _bar.update(percent/100);
-        },
-        "path": "/Users/hano/Desktop/TestApp1",
-        "url": server.baseurl,
-        "auth": {
-            "user": server.username + ':mway',
-            "pass": server.password
-        },
-        "deleteMissing": "",
-        "remotePath": "/applications/",
-        "log": false
-    };
+    var validation = mcapApplicationValidation.validate(dir);
+    if (validation === true) {
+        var _bar = bar.create(process.stdout);
+        _bar.format = '$bar; $percentage,2:0;% uploaded.';
+        _bar.symbols.loaded	= '#';
+        _bar.symbols.notLoaded	= '-';
+        _bar.width = 50;
+        var uploadConfig = {
+            progress: function (percent) {
+                _bar.update(percent / 100);
+            },
+            "path": dir,
+            "url": server.baseurl,
+            "auth": {
+                "user": server.username,
+                "pass": server.password
+            },
+            "deleteMissing": "",
+            "overwrite": true,
+            "remotePath": "/applications",
+            "log": false
+        };
 
-    mcapFileExchange.upload(uploadConfig).then(function( data ) {
-        console.log('\nSuccessfully Uploaded');
-    }).catch(function( e ) {
-        console.log('error', e);
-    });
+        mcapFileExchange.upload(uploadConfig).then(function (data) {
+            try {
+                var _data = JSON.parse(data);
+                console.log('\nUploaded state:' + _data.itemStatuses[0].severity)
+            } catch (e) {
 
+            }
+        }).catch(function (e) {
+            console.log('\nerror', e);
+        });
+    }
+    else {
+        console.error(validation);
+    }
 }
 
 program
@@ -76,7 +96,7 @@ program
     .option('server <list>', 'List all server')
     .option('server <default> [alias]', 'List all server')
     .option('log [alias]', 'Live logger of the given server')
-    .option('deploy [alias]', 'Deploy the application to the given server')
+    .option('deploy [alias] [path]', 'Deploy the application to the given server')
     .option('generate <component>', 'Generate a mCAP Component, components: ' + component.getComponentList().join(', '))
     .parse(process.argv);
 
