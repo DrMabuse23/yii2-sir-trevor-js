800e3a05d4b0d2dc2106c7af88ccfc9dc33f667a
Marco Hanowski <its@time2dev.com>
2014-09-23 14:28:09 +0200

    add command serve

--- cli.js
+++ cli.js
@@ -6,6 +6,7 @@ var cmdComponent = require('./lib/command/component');
 var cmdNew = require('./lib/command/new');
 var cmdServer = require('./lib/command/server');
 var cmdDeploy = require('./lib/command/deploy');
+var cmdServe = require('./lib/command/serve');
 var cmdPublish = require('./lib/command/publish');
 var cmdLog = require('./lib/command/log');
 var checkProject = require('./lib/check_project');
@@ -28,6 +29,7 @@ program
     .option('log [alias]', 'Live logger of the given server')
     .option('deploy [alias] [path]', 'Deploy the application to the given server')
     .option('publish [alias] [releaseStatus] [archivationMode]', 'Publish the Client Application to Relution. Defaults: releaseStatus: DEVELOPMENT archivationMode: archive')
+    .option('serve [alias]', 'Starts a local webserver and serves the client app with live reload. Set an alias for DataSync requests.')
     .option('generate <component>', 'Generate a mCAP Component. Available components: ' + cmdComponent.getComponentList().join(', '))
     .parse(process.argv);
 
@@ -47,6 +49,9 @@ else if (program.server) {
 }
 else if (program.publish) {
     cmdPublish.publish(program);
+}
+else if (program.serve) {
+    cmdServe.serve(program);
 }
 else if (program.log) {
     if (checkProject.isInsideProject(true)) {
